6e599f99ea6fbd27a96cbcd74f707f66b0533f50
Andrew Dirksen <andrew@dirksen.com>
2020-04-01 10:48:20 -0700

    Revert "remove custom excpetion"
    This reverts commit 78f9ccb2966a6110d148478a165e158c84a943bb.

--- src/resolver.js
+++ src/resolver.js
@@ -21,7 +21,11 @@ class Resolver {
   constructor(providers, universalResolverUrl) {
     // If `universalResolverUrl` is passed, ensure that it is a URL
     if (universalResolverUrl) {
-      new URL(universalResolverUrl);
+      try {
+        new URL(universalResolverUrl);
+      } catch (e) {
+        throw new Error('Invalid URL given for universal resolver. Parsing resulted in the following error', e);
+      }
       // Remove trailing slash if any and append the string `/1.0/identifiers/`
       this.universalResolverUrl = `${universalResolverUrl.replace(/\/$/, '')}/1.0/identifiers/`;
     }
